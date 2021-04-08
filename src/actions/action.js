7293b81ed8ec43aed0f8be32599e06f2a503a944
Precious chiedu <udegbue69@gmail.com>
2021-04-08 22:59:04 +0100

    styled button

--- src/actions/action.js
+++ src/actions/action.js
@@ -1,26 +1,23 @@
 const setResult = (payload) => ({ type: "SET_RESULT", payload });
-// eslint-disable-next-line
-const resultError = (error) => ({ type: "SET_ERROR", error });
-
-const PROXY_URL = 'https://bellroy-test.herokuapp.com/';
-
 
 const submitResult =
   "https://recruitbot.trikeapps.com/api/v1/roles/bellroy-tech-team-recruit/big_five_profile_submissions.";
 
-export const userResult = (userInfo) => async (dispatch) => {
-  await fetch(submitResult, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json",
-    },
-
-    body: JSON.stringify(userInfo),
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      dispatch(setResult(data));
-    });
-};
+  export const userResult = (userInfo) => async (dispatch) => {
+    await fetch(submitResult, {
+      method: "POST",
+      // mode: 'no-cors',
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify(userInfo)
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        dispatch(setResult(data))})
+      .catch(error => {
+        console.log(error)
+      });
+  };
