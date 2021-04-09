e03bf61bc79e2a0f850fff3df67a1c15623536a3
Precious chiedu <udegbue69@gmail.com>
2021-04-09 10:45:35 +0100

    used proxy server

--- src/actions/action.js
+++ src/actions/action.js
@@ -1,23 +1,47 @@
+import axios from "axios";
 const setResult = (payload) => ({ type: "SET_RESULT", payload });
 
 const submitResult =
   "https://recruitbot.trikeapps.com/api/v1/roles/bellroy-tech-team-recruit/big_five_profile_submissions.";
 
-  export const userResult = (userInfo) => async (dispatch) => {
-    await fetch(submitResult, {
-      method: "POST",
-      // mode: 'no-cors',
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-      },
-      body: JSON.stringify(userInfo)
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        dispatch(setResult(data))})
-      .catch(error => {
-        console.log(error)
-      });
-  };
+export const userResult = (userInfo) => async (dispatch) => {
+  await fetch(submitResult, {
+    method: "POST",
+    // mode: 'no-cors',
+    headers: {
+      'Accept': 'application/json',
+      "Content-type": "application/json; charset=UTF-8",
+      "Access-Control-Allow-Methods": 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+      "Access-Control-Allow-Origin" : "*", 
+    },
+    body: JSON.stringify(userInfo)
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      dispatch(setResult(data))})
+    .catch(error => {
+      console.log(error)
+    });
+};
+
+// export const userResult = (userInfo) => async (dispatch) => {
+//   await axios
+//     .post(submitResult, {
+//       headers: {
+//         "Content-Type": "application/json",
+//         "Access-Control-Allow-Origin": "*",
+
+//       },
+//       body: {
+//         userInfo
+//       },
+//     })
+//     .then((data) => {
+//       console.log(data);
+//       dispatch(setResult(data));
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// };
--- src/component/formSubmit.js
+++ src/component/formSubmit.js
@@ -4,48 +4,48 @@ import "./form.css";
 import { userResult } from "../actions/action";
 
 const initialState = {
-  "NAME": "Precious",
-  "EMAIL": "udegbue69@gmail.com",
+  "NAME":"Precious",
+  "EMAIL":"udegbue69@gmail.com",
   "EXTRAVERSION": {
-    "Overall Score": 45,
+    "Overall Score":45,
     "Facets": {
-      "Friendliness": 56,
-      "Gregariousness": 55,
-      "Assertiveness": 34,
-      "Activity Level": 60,
-      "Excitement-Seeking": 17,
-      "Cheerfulness": 57
+      "Friendliness":56,
+      "Gregariousness":55,
+      "Assertiveness":34,
+      "Activity Level":60,
+      "Excitement-Seeking":17,
+      "Cheerfulness":57
     }
   },
   "AGREEABLENESS": {
-    "Overall Score": 93,
+    "Overall Score":93,
     "Facets": {
-      "Trust": 61,
-      "Morality": 89,
-      "Altruism": 95,
-      "Cooperation": 80,
-      "Modesty": 45,
-      "Sympathy": 99
+      "Trust":61,
+      "Morality":89,
+      "Altruism":95,
+      "Cooperation":80,
+      "Modesty":45,
+      "Sympathy":99
     }
   },
   "CONSCIENTIOUSNESS": {
-    "Overall Score": 88,
+    "Overall Score":88,
     "Facets": {
-      "Self-Efficacy": 52,
-      "Orderliness": 98,
-      "Dutifulness": 88,
-      "Achievement-Striving": 66,
-      "Self-Discipline": 68,
-      "Cautiousness": 72
+      "Self-Efficacy":52,
+      "Orderliness":98,
+      "Dutifulness":88,
+      "Achievement-Striving":66,
+      "Self-Discipline":68,
+      "Cautiousness":72
     }
   },
   "NEUROTICISM": {
-    "Overall Score": 7,
+    "Overall Score":7,
     "Facets": {
-      "Anxiety": 12,
+      "Anxiety":12,
       "Anger": 27,
-      "Depression": 38,
-      "Self-Consciousness": 1,
+      "Depression":38,
+      "Self-Consciousness":1,
       "Immoderation": 12,
       "Vulnerability": 19
     }
@@ -53,10 +53,10 @@ const initialState = {
   "OPENNESS TO EXPERIENCE": {
     "Overall Score": 40,
     "Facets": {
-      "Imagination": 19,
-      "Artistic Interests": 60,
+      "Imagination":19,
+      "Artistic Interests":60,
       "Emotionality": 65,
-      "Adventurousness": 38,
+      "Adventurousness":38,
       "Intellect": 18,
       "Liberalism": 62
     }
